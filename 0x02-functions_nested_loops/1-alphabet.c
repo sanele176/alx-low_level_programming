269e1c817a8e963f540f0fd6b0b6376df79e5a27
Mancoba Dlamini <mancobasanele@gmail.com>
2023-03-21 10:29:23 -0700

    Write a program that prints _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+/**
+ * print_alphabet - prints the english alphabet from a-z.
+ * Return: Nothing.
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
