bc4de552d127100f8c4daf98a9ce81016c405ba0
Mancoba Dlamini <mancobasanele@gmail.com>
2023-04-03 12:21:52 -0700

    Write a function that locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,21 +10,27 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int index;
+
 	if (*needle == 0)
+
 		return (haystack);
+
 	while (*haystack)
 	{
 		index = 0;
+
 		if (haystack[index] == needle[index])
 		{
 			do {
-				if (needle[index +1] == '\0')
-						return (haystack);
-			         index++;
-				} while (haystack[index] == needle[index]);
-					}
+				if (needle[index + 1] == '\0')
+					return (haystack);
 
-					haystack++;
-					}
-					return ('\0');
+				index++;
+
+			} while (haystack[index] == needle[index]);
+		}
+
+		haystack++;
+	}
+	return ('\0');
 }
